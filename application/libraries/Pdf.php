34910b0b1832e3d533e889608449de533e1ee850
grebo87 <grebo87@gmail.com>
2015-05-29 15:55:48 -0430

    libreria TCPDF para generar archivos en formato .pdf

---
+++ application/libraries/Pdf.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once dirname(__FILE__) . '/tcpdf/tcpdf.php';
+
+class Pdf extends TCPDF
+{
+
+
+
+    public function Header() {
+        /*puedes modificarla cuando la uses */
+
+
+    }
+     public function Footer() {
+       /*puedes modificarla cuando la uses */
+
+
+    }
+}
+/* application/libraries/Pdf.php */
